04908589b2bee6928f4c51ff3f5c41662fb76f5f
Luna Feng <luna.feng@thomsonreuters.com>
2018-12-31 21:43:50 -0500

    Update binearySearchLoop.py

--- binearySearchLoop.py
+++ binearySearchLoop.py
@@ -2,24 +2,26 @@ class BinearySearch:
     def search(self, arr, target):
         start = 0
         end = len(arr) - 1
-        return self.helper(arr, start, end, target)
+        res = self.helper(arr, start, end, target)
+        if res is None:
+            print "Target not found"
+        else:
+            print "Target found at index: ", res
 
     def helper(self, arr, start, end, target):
         while(start <= end):
             mid = (start + end)/2
             elem = arr[mid]
             if elem == target:
-                return "Found target at index: ", mid
+                return mid
             elif elem > target:
                 end = mid
-                self.helper(arr, start, end, target)
             else:
                 start = mid + 1
-                self.helper(arr, start, end, target)
-        return "Target not found"
+        return
 
 
 
 test = BinearySearch()
 arr = [1,2,3,4,5,6,7,8,9]
-print test.search(arr, 1)
+test.search(arr, 6)
